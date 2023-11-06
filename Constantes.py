cafb53fc320bfb6c61f5d457d68bf63152c0e2e8
antoine joffroy <antoine.joffroy@mines-ales.org>
2023-11-06 12:50:48 +0100

    final work

---
+++ Constantes.py
@@ -0,0 +1,9 @@
+#forced max swap parameters
+FORCED_MAX_SWAP_PRECISION = 1.5
+FORCED_MAX_SWAP_TRY = 20
+
+# Cluster parameters
+MIN_CLUSTER_RANGE = 5
+MAX_CLUSTER_RANGE = 6
+#instance parameter
+INSTANCE_PATH = 0
